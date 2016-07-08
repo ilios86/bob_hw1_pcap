00c7c94167cab04380d819ecb939cd659669dd07
Yonggon Kim <ilios86@gmail.com>
2016-07-08 20:34:59 +0900

    remove user file

--- hw_pcap/main.cpp
+++ hw_pcap/main.cpp
@@ -180,8 +180,8 @@ void fprint_hex(FILE *fd, const u_char *addr, int len) {
 void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data)
 {
 
-    FILE *fd = fopen("packet.log", "a");
-//    FILE *fd = stdout;
+//    FILE *fd = fopen("packet.log", "a");
+    FILE *fd = stdout;
 
 
     int ethernet_header_len, ip_header_len, tcp_header_len;
